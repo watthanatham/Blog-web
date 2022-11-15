a7abb4b617743af3f27eba13248fcc9d50490ad1
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-11-15 21:20:56 +0700

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
+# import django class
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
+
+# import models
 from .models import Post
 
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(status=True)
-  return render(request, 'index.html', {'posts': posts})
+  
+  paginator = Paginator(posts, 3)
+  
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+  
+  return render(request, 'index.html', {'page_obj': page_obj})
+  
 
 def post_detail(request, id):
   post = Post.objects.get(id=id, status=True)
