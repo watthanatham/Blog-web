7e8ac151b6dec907d8bafbb6b86db0f313207967
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-11-15 18:17:55 +0700

    get blog by id, add post_detail page, base html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('post/<int:id>/', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def index(request):
   posts = Post.objects.all()
   return render(request, 'index.html', {'posts': posts})
+
+def post_detail(request, id):
+  post = Post.objects.get(id=id)
+  return render(request, 'post_detail.html', {'post': post})
